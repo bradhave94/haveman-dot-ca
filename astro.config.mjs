66926cfd2fe8533ea5b772d1946d91bbdd3b2e44
Bradley Haveman <bradley@haveman.ca>
2025-06-30 14:31:20 -0400

    added tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import tailwindcss from '@tailwindcss/vite'
 
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
@@ -7,6 +8,9 @@ import pagefind from "astro-pagefind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://haveman.ca",
+  vite: {
+    plugins: [tailwindcss()],
+  },
   integrations: [sitemap(), mdx(), pagefind()],
   markdown: {
     shikiConfig: {
